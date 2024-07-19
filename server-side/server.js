267860259e21e2bfa92fb1193e86fbed72327e59
RameshBellani <136491880+RameshBellani@users.noreply.github.com>
2024-07-19 16:13:05 +0530

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -1,4 +1,4 @@
-// server.js
+
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
@@ -8,21 +8,21 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Connect to MongoDB
+
 connectDB();
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
 
-// Passport middleware
+
 require('./config/passport')(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Routes
+
 app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
